e657756a9293500543c09c9114b278626cd19947
Artem <blbo@yandex.ru>
2023-08-15 14:58:58 +0400

    Правки слайдера

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,39 +1,37 @@
-const slider = document.querySelector(".items");
-		const slides = document.querySelectorAll(".item");
-		const button = document.querySelectorAll(".button");
+const slider = document.querySelector(".carousel__items");
+const slides = document.querySelectorAll(".carousel__item");
+const button = document.querySelectorAll(".carousel__button");
 
-		let current = 0;
-		let prev = 4;
-		let next = 1;
+let current = 0;
+let prev = 4;
+let next = 1;
 
-		for (let i = 0; i < button.length; i++) {
-			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-		}
+for (let i = 0; i < button.length; i++) {
+	button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
 
-		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+const gotoNum = number => {
+	current = number;
+	prev = current - 1;
+	next = current + 1;
 
-		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+	for (let i = 0; i < slides.length; i++) {
+		slides[i].classList.remove("carousel__active");
+		slides[i].classList.remove("carousel__prev");
+		slides[i].classList.remove("carousel__next");
+	}
 
-		const gotoNum = number => {
-			current = number;
-			prev = current - 1;
-			next = current + 1;
+	if (next == 5) {
+		next = 0;
+	}
 
-			for (let i = 0; i < slides.length; i++) {
-				slides[i].classList.remove("active");
-				slides[i].classList.remove("prev");
-				slides[i].classList.remove("next");
-			}
+	if (prev == -1) {
+		prev = 4;
+	}
 
-			if (next == 5) {
-				next = 0;
-			}
-
-			if (prev == -1) {
-				prev = 4;
-			}
-
-			slides[current].classList.add("active");
-			slides[prev].classList.add("prev");
-			slides[next].classList.add("next");
-		}
+	slides[current].classList.add("carousel__active");
+	slides[prev].classList.add("carousel__prev");
+	slides[next].classList.add("carousel__next");
+}
